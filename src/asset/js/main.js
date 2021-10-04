e64d05323a2006c7bb2e0290ab2dd68cd8a92c8d
paulaciddelprado <cid.6924@gmail.com>
2021-10-04 00:32:36 -0500

    Archivos NPM

---
+++ src/asset/js/main.js
@@ -0,0 +1,9 @@
+import Swal from 'sweetalert2';
+import "animate.css";
+
+Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  })
